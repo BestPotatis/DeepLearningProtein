de81c0a0b4798fcacdaa00e7bbb61ebb0a35c86c
Olivers194606 <s194606@student.dtu.dk>
2023-11-06 16:06:34 +0100

    Testing graph and readme updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DeepLearningProtein
-# DeepLearningProtein
+## Installation
+https://graphein.ai/getting_started/installation.html
+
+## Data
+https://drive.google.com/file/d/10W1sECBtNirdVEyQFeld-0H8gn4Od1sH/view?usp=sharing
--- TestingGraph.py
+++ TestingGraph.py
@@ -1,6 +1,8 @@
 from graphein.protein.config import ProteinGraphConfig
 from graphein.protein.graphs import construct_graph
 from graphein.protein.visualisation import plotly_protein_structure_graph
+import matplotlib.pyplot as plt
+import networkx as nx
 
 config = ProteinGraphConfig()
 
@@ -12,12 +14,13 @@ g = construct_graph(config=config, path="data/"+proteinName+".pdb")
 # https://networkx.org/documentation/stable/tutorial.html
 
 # Get nodes and edges
-#g.nodes
-#g.edges 
+#print(g.nodes)
+#print(g.edges)
 
 # Visualize the graph
 # https://graphein.ai/notebooks/alphafold_protein_graph_tutorial.html
-plotly_protein_structure_graph(g, node_size_multiplier=0.5, colour_nodes_by="residue_name")
+plot = plotly_protein_structure_graph(g, node_size_multiplier=0.2, label_node_ids=False, colour_nodes_by="residue_name")
+plot.show()
 
 # Graph analysis
 # https://graphein.ai/notebooks/protein_graph_analytics.html
